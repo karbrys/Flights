eccf74c73a9c52b61fc62773e949d029fa06a17a
Karolina Bryś <56247992+karbrys@users.noreply.github.com>
2021-03-27 14:48:51 +0100

    Project

--- struktury.h
+++ struktury.h
@@ -1,31 +1,23 @@
-/** @file */
-//  struktury.h
-//  LOTY
-//  Created by Karola on 29/12/2019.
-//  Copyright © 2019 Karolina Bryś. All rights reserved.
-
 #ifndef struktury_h
 #define struktury_h
 
 #include <string>
 using namespace std;
 
-/** Element listy jednokierunkowej "lista_pasazerow"  */
 struct pasazer
 {
-    string nazwisko_pasazera;   ///Wartość przechowywana w liście symbolizująca nazwisko danego pasażera.
-    int numer_miejsca;          ///Wartość przechowywana w liście symbolisująca numer miejsca danego pasażera.
-    pasazer *nastepny_pasazer;  ///Wskaźnik na następny element listy jednokierunkowej.
+    string nazwisko_pasazera;   
+    int numer_miejsca;          
+    pasazer *nastepny_pasazer;  
 };
 
-/** Element listy jednokierunkowej "lista_lotow"  */
 struct lot
 {
-    string symbol_lotu;         ///Wartość przechowywana w liście symbolizująca symbol danego lotu.
-    string lotnisko_startowe;   ///Wartość przechowywana w liście symbolizująca  lotnisko startowe danego lotu.
-    string data_lotu;           ///Wartość przechowywana w liście symbolizująca datę danego lotu.
-    lot *nastepny_lot;          ///Wskaźnik na następny element listy jednokierunkowej "lista_lotow".
-    pasazer *lista_pasazerow;   ///Wskaźnik na następny element listy jednokierunkowej "lista_pasazerow".
+    string symbol_lotu;         
+    string lotnisko_startowe;   
+    string data_lotu;           
+    lot *nastepny_lot;         
+    pasazer *lista_pasazerow;   
 };
 
 #endif 
